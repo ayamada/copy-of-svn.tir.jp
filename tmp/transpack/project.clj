119988df6e562f6e15a785dafcf0c870bdc2e1a1
atsuo yamada <yamada-git@tir.jp>
2014-03-09 00:04:16 +0900

    moved

--- tmp/transpack/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject vnctst.transpack "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.taoensso/nippy "2.5.2"]
-                 ])
